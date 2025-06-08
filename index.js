bd3f68ae601069d130de4bfd2b649bdd048e4ab6
theharshk <kharshmolu@gmail.com>
2025-06-08 09:01:48 +0530

    Push chat_backend

---
+++ index.js
@@ -0,0 +1,34 @@
+// Node server which whill handle socket io connections and serve the frontend files
+const io = require('socket.io')(8000, {
+    cors: {
+        origin: "http://127.0.0.1:5500",
+        methods: ["GET", "POST"]
+    }
+});
+
+const user = {};
+
+io.on('connection', socket => {
+    //IF NAY USER JOINS LET ANY USER KNOW WHO HAS JOINED
+    socket.on('new-user-joined', name => {
+        console.log("New user ", name);
+        user[socket.id] = name;
+        socket.broadcast.emit('user-joined', name);
+    });
+    //IF SOMEONE SENDS A MESSAGE BORADCAST IT TO ANOTHER PEOPLE
+    socket.on('send', message => {
+        socket.broadcast.emit('receive', { message: message, name: user[socket.id] })
+    });
+    // socket.on('disconnect', message => {
+    //     socket.broadcast.emit('left', user[socket.id]);
+    //     delete user[socket.id];
+    // });
+
+    //IF SOMEONE LEAVES THE CHAT LET OTHERS KNOW
+    socket.on('disconnect', () => {
+        const username = user[socket.id];
+        socket.broadcast.emit('left', username);
+        delete user[socket.id];
+        console.log("User disconnected:", username);
+    });
+});
